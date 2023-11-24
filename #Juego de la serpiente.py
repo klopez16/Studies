34ffcebabe17a65588e851c466570391d4944f58
klopez16 <klopezb16@outlook.com>
2023-11-24 16:47:38 -0500

    Inicio de configuracion de la pantalla

--- #Juego de la serpiente.py
+++ #Juego de la serpiente.py
@@ -5,9 +5,17 @@ import time                                         #Usamos la libreria time par
 import random                                       #Usamos la libreria random para usar numeros aleatorios
 
 # Configuración de la ventana
+window = turtle.Screen()
+turtle.window_width()                               #Dimensiones de la pantalla (ancho)
+900                                                 
+turtle.window_height()                              #Dimensiones de la pantalla (alto)
+900                                                 
+turtle.delay()                                      #Establecer un retraso entre actualizaciones del lienzo
+500
+window.tracer(0)
 
-WIDTH, HEIGHT = 900, 900                            #Dimensiones de la pantalla    
-SCREEN_TITLE = "Juego de la Serpiente"              #Título de la pantalla o del juego
-DELAY = 0.1                                         
-PUNTAJE = 0                                         #El mensaje del puntaje de la partida actuale iniciará en 0
-MAXPUNTAJE = 0                                      #El mensaje del puntaje máximo iniciará en 0
+#Configuración de variables
+window.title('Snake Game')                          #Título del juego que se visualiza en la pantalla  
+window.bgcolor("black")
+Puntaje = 0                                         #El mensaje del puntaje de la partida actuale iniciará en 0
+MaxPuntaje = 0                                      #El mensaje del puntaje máximo iniciará en 0
