f5b593ad263051a2a02eda1fd41b6b6a251a0449
klopez16 <klopezb16@outlook.com>
2023-12-17 22:28:39 -0500

    Avance general y correcciones en pantalla

--- #Juego de la serpiente.py
+++ #Juego de la serpiente.py
@@ -4,10 +4,8 @@ import turtle                                       #Libreria para construir la 
 import random                                       #Libreria para traer numeros aleatorios (en este caso lo utilizo para generar aleatoriamente las coordenadas del alimento)
 import time                                         #Libreria para acceder a funciones relacionadas con el tiempo
 
-
 #Configuracion pantalla
 hold = 0.1                                          #Variable asignada para determinar un tiempo, en este caso un retraso 
-
 
 # Configuración de la pantalla
 window = turtle.Screen()                            #Pantalla a generarse
@@ -18,14 +16,12 @@ turtle.window_height()                              #Dimensiones de la pantalla 
 turtle.delay()                                      #Establecer un retraso entre actualizaciones del lienzo
 500
 window.tracer(0)                                    #Establecer un mejor visual para la animacion
-
-#Configuración de variables de la pantalla
 window.title('Snake Game')                          #Título del juego que se visualiza en la pantalla  
 window.bgcolor('white')
 
 #Configuracion de la serpiente (cabeza)
 head = turtle.Turtle()                              #Asignar una variable de un objeto creado en turtle en la pantalla (en este caso la cabeza)
-head.speed(6)                                       #Establecer la velocidad de la animacion en turtle
+head.speed(0)                                       #Establecer la velocidad de la animacion en turtle
 head.shape('circle')                                #Establecer una forma geometrica para la cabeza de la serpiente
 head.color('blue')                                  #Establecer un color para la cabeza de la serpiente
 head.penup()                                        #Funcion para borrar el rastro de la cabeza en la pantalla
@@ -34,67 +30,55 @@ head.direction = 'stop'                             #Variable para solicitar que
 
 #Configuracion del alimento
 food = turtle.Turtle()                              #Asignar una variable de un objeto creado en turtle en la pantalla (en este caso el alimento)
-food.speed(6)                                       #Establecer la velocidad de la animacion en turtle
+food.speed(0)                                       #Establecer la velocidad de la animacion en turtle
 food.shape('triangle')                              #Establecer una forma geometrica para el alimento de la serpiente
 food.color('green')                                 #Establecer un color para el alimento de la serpiente
 food.penup()                                        #Funcion para borrar el rastro de el alimento en la pantalla
-food.goto(0,0)                                      #Funcion aplicada para que el alimento aparezce una parte aleatoria de la pantalla
+food.goto(0,100)                                      #Funcion aplicada para que el alimento aparezce una parte aleatoria de la pantalla
 
-#Configuracion del texto del puntaje
-point = turtle.Turtle()                                                     #Asignar una variable de un objeto creado en turtle en la pantalla (en este caso el texto del puntaje)
-point.speed(6)                                                              #Establecer la velocidad de la animacion en turtle
-point.color('black')                                                        #Establecer un color para el alimento de la serpiente
-point.penup()                                                               #Funcion para borrar el rastro de el alimento en la pantalla
-point.hideturtle()                                                          #Funcion para ocultar el objeto cuando se dibuja otros
-point.goto(0,500)                                                           #Funcion aplicada para que el alimento aparezce una parte aleatoria de la pantalla                                     
-point.write('Puntaje:0', align='center', font=('Arial', 25, 'normal'))      #Funcion para visualizar el texto en la pantalla
-point = 0                                                                   #El mensaje del puntaje de la partida actuale iniciará en 0
-
-#Configuracion del cuerpo de la serpiente
-body = []
-color = [(109,160,104),(104,142,160)]
-
-def crearbody():                                    #Funcion para reservar un bloque de codigo definiendo el nombre asignado al bloque
-    body = turtle.Turtle()                          #Asignar una variable de un objeto creado en turtle en la pantalla (en este caso el cuerpo)
-    turtle.colormode(1)                             #Funcion para determinar el rango numerico de los colores (RGB)
-    body.speed(6)                                   #Establecer la velocidad de la animacion en turtle
-    body.shape('circle')                            #Establecer una forma geometrica para el cuerpo de la serpiente
-    body.color(random.choice(color))                #Funcion para generar aleatoriamente los colores del cuerpo de la serpiente
-    body.penup()                                    #Funcion para borrar el rastro de el alimento en la pantalla
-    body.append(body)                               #Funcion para agregar objetos a otro definido
-    point += 1
-    printText()
+segments = []
 
 #Definicion de movimientos
 def arriba ():                                      #Funcion para reservar un bloque de codigo definiendo el nombre asignado al bloque
-    head.direction = 'up'                           #Definimos la variable con direccion hacia arriba
+    if head.direction != 'Down':                    #Definimos la variable con direccion hacia arriba
+        head.direction = 'Up'
 
 def abajo ():                                       #Funcion para reservar un bloque de codigo definiendo el nombre asignado al bloque
-    head.direction = 'down'                         #Definimos la variable con direccion hacia abajo
+    if head.direction != 'Up':                      #Definimos la variable con direccion hacia abajo
+        head.direction = "Down"
 
 def izquierda ():                                   #Funcion para reservar un bloque de codigo definiendo el nombre asignado al bloque
-    head.direction = 'left'                         #Definimos la variable con direccion hacia izquierda
+    if head.direction != 'right':                   #Definimos la variable con direccion hacia izquierda
+        head.direction = 'left'
 
 def derecha ():                                     #Funcion para reservar un bloque de codigo definiendo el nombre asignado al bloque
-    head.direction = 'right'                        #Definimos la variable con direccion hacia derecha
+    if head.direction != 'left':                     #Definimos la variable con direccion hacia derecha
+        head.direction = 'right'
 
 #Configuracion para ejecucion de movimiento
 def direction():                                    #Funcion para reservar un bloque de codigo definiendo el nombre asignado al bloque
     if head.direction == "up":                      #Funcion IF para determinar hacia donde se presenta la direccion
         y =  head.ycor()                            #Definimos a Y como la variable que determina el movimiento vertical de la serpiente 
-        head.sety(y + 25)                           #Funcion para actualizar (mover) la posicion de cabeza de la serpiente en la coordenada Y
+        head.sety(y + 20)                           #Funcion para actualizar (mover) la posicion de cabeza de la serpiente en la coordenada Y
 
     elif head.direction == "down":                  #Funcion ELIF para determinar hacia donde se presenta la direccion
         y =  head.ycor()                            #Definimos a Y como la variable que determina el movimiento vertical de la serpiente 
-        head.sety(y - 25)                           #Funcion para actualizar (mover) la posicion de cabeza de la serpiente en la coordenada Y
+        head.sety(y - 20)                           #Funcion para actualizar (mover) la posicion de cabeza de la serpiente en la coordenada Y
 
     elif head.direction == "left":                  #Funcion ELIF para determinar hacia donde se presenta la direccion
         x =  head.xcor()                            #Definimos a X como la variable que determina el movimiento horizontal de la serpiente 
-        head.setx(x - 25)                           #Funcion para actualizar (mover) la posicion de cabeza de la serpiente en la coordenada X
+        head.setx(x - 20)                           #Funcion para actualizar (mover) la posicion de cabeza de la serpiente en la coordenada X
 
     elif head.direction == "right":                 #Funcion ELIF para determinar hacia donde se presenta la direccion
         x =  head.xcor()                            #Definimos a X como la variable que determina el movimiento horizontal de la serpiente 
-        head.setx(x + 25)                           #Funcion para actualizar (mover) la posicion de cabeza de la serpiente en la coordenada X
+        head.setx(x + 20)                           #Funcion para actualizar (mover) la posicion de cabeza de la serpiente en la coordenada X
+
+#Configuracion para conexion de la pantalla con el teclado
+    window.listen()                                     #Funcion donde la pantalla está esperando la accion
+    window.onkeypress(arriba,'Up')                      #Funcion que ejecuta la reserva del bloque definido, en este caso hacia arriba
+    window.onkeypress(abajo,'Down')                     #Funcion que ejecuta la reserva del bloque definido, en este caso hacia abajo
+    window.onkeypress(izquierda,'Left')                 #Funcion que ejecuta la reserva del bloque definido, en este caso hacia la izquierda
+    window.onkeypress(derecha,'Right')                  #Funcion que ejecuta la reserva del bloque definido, en este caso hacia la derecha
 
 #Configuracion para movimiento del cuerpo
 def movementBody():                                 #Funcion para reservar un bloque de codigo definiendo el nombre asignado al bloque
@@ -105,10 +89,10 @@ def movementBody():                                 #Funcion para reservar un bl
         y = body[body-1].ycor()                     #Método para obtener la coordenada Y del objeto
         body[body].goto(x,y)                        #Método para mover el objeto a las coordenadas X,Y obtenidas en las dos líneas anteriores
 
-    if totalBody >0:
-        x = head.xcor()
-        y = head.ycor()
-        body[0].goto(x,y)
+    if totalBody >0:                                #Funcion IF para determinar hacia donde se presenta la direccion
+        x = head.xcor()                             #Definimos a X como la variable que determina el movimiento horizontal de la serpiente
+        y = head.ycor()                             #Definimos a Y como la variable que determina el movimiento vertical de la serpiente
+        body[0].goto(x,y)                           #Método para mover el objeto a las coordenadas X,Y
 
 #Configuracion de Alimentacion (choque de la cabeza con la comida)
 def feeding():                                      #Funcion para reservar un bloque de codigo definiendo el nombre asignado al bloque
@@ -116,16 +100,6 @@ def feeding():                                      #Funcion para reservar un bl
         x = random.randint(-500, 500)               #Funcion para generar un numero aleatorio en el rango establecido para la coordenada X
         y = random.randint(-500, 500)               #Funcion para generar un numero aleatorio en el rango establecido para la coordenada Y
         food.goto(x,y)                              #Actualizacion del alimento de manera random
-        newBody()                                   #Nueva variable para el cuerpo
-
-
-
-#Configuracion para conexion de la pantalla con el teclado
-window.listen()                                     #Funcion donde la pantalla está esperando la accion
-window.onkeypress(arriba,'Up')                      #Funcion que ejecuta la reserva del bloque definido, en este caso hacia arriba
-window.onkeypress(abajo,'Down')                     #Funcion que ejecuta la reserva del bloque definido, en este caso hacia abajo
-window.onkeypress(izquierda,'Left')                 #Funcion que ejecuta la reserva del bloque definido, en este caso hacia la izquierda
-window.onkeypress(derecha,'Right')                  #Funcion que ejecuta la reserva del bloque definido, en este caso hacia la derecha
 
 #Configuracion de la pantalla para que sea continua
 while True:                                         #Funcion para crear un bucle infinito en este caso utilizada para que la pantalla se mantenga activa
